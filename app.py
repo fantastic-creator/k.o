f49406d1f82908b6a0af5e84216a2ebbec0f38d7
fantastic=creator <1209987110@qq.com>
2025-02-12 02:32:21 +0800

    第一次提交

---
+++ app.py
@@ -0,0 +1,11 @@
+students_count = 1000
+rating = 4.99
+is_pubilished = True
+course = "Python Programming"
+print(students_count)
+print(len(course))
+print(course[0])
+print(course[0:3])
+print(course[0:])
+x = input("x: ")
+y = x+1
